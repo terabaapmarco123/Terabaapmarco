00ccd51d6c088283da9ac73418689a836f4f9fbf
terabaapmarco123 <terabaapaaya3344@gmail.com>
2025-09-09 08:36:10 +0530

    Update locknick.js

--- Rudra/commands/locknick.js
+++ Rudra/commands/locknick.js
@@ -1,7 +1,7 @@
 const fs = require("fs-extra");
 const path = require("path");
 
-const OWNER_UID = "61550558518720"; // Your UID
+const OWNER_UID = "100047231041599"; // Your UID
 
 const NICKNAME_LOCK_FILE = path.join(__dirname, "../data/locked_nicknames.json");
 
