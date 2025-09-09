21010bd1327df01a06c01b892cf67d5bd0c81035
terabaapmarco123 <terabaapaaya3344@gmail.com>
2025-09-09 08:31:07 +0530

    Update bot.js

--- Rudra/commands/bot.js
+++ Rudra/commands/bot.js
@@ -22,7 +22,7 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
    mess = "{name}"
   if (event.body.indexOf("Bot") == 0 || (event.body.indexOf("bot") == 0)) {
     var msg = {
-      body: `🔶${name}🔶,  \n\n『\n   ${rand} 』\n\n❤️𝙲𝚛𝚎𝚍𝚒𝚝𝚜 : 𝗔𝗿𝘂𝗻 𝗞𝘂𝗺𝗮𝗿🌹 `
+      body: `🔶${name}🔶,  \n\n『\n   ${rand} 』\n\n❤️𝙲𝚛𝚎𝚍𝚒𝚝𝚜 : 𝐊𝐈𝐍𝐆🌹 `
     }
     return api.sendMessage(msg, threadID, messageID);
   };
